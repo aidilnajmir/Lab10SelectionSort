84101b77ef3f2d3e3f0c642fa0d355af6b07f28d
Mohamad Aidil Najmir <aidilnaj@umich.edu>
2023-04-13 22:09:57 -0400

    Bob add testMixed and testDuplicates

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -45,12 +45,46 @@ public class testSelectionSort {
 	}
 	
 	public void testMixed(){
+		/** Test data contains with both positive, negative and zeros **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = 0;
+		arr[3] = 10;
+		arr[4] = 2;
 
-	/** Test data contains with both positive, negative and zeros **/
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -9;
+		Sortedarr[1] = -8;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 2;
+		Sortedarr[4] = 10;
+
+		/** add tests to check for this unit test **/
+		SelectionSort ss = new SelectionSort();
+		int sortedActual[] = ss.basicSelectionSort(arr);
+	    	assertArrayEquals("wrong answer", Sortedarr, sortedActual);
 	}
 
 	public void testDuplicates(){
-	
-	/** Test data contains duplicates **/
+		/** Test data contains duplicates **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -8;
+		arr[2] = 2;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -8;
+		Sortedarr[1] = -8;
+		Sortedarr[2] = 2;
+		Sortedarr[3] = 2;
+		Sortedarr[4] = 10;
+
+		/** add tests to check for this unit test **/
+		SelectionSort ss = new SelectionSort();
+		int sortedActual[] = ss.basicSelectionSort(arr);
+	    	assertArrayEquals("wrong answer", Sortedarr, sortedActual);	
 	}
 }
